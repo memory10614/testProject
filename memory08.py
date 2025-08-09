b170d2bae07c95fea3fc7fac2819bbb52e2227c8
memory10614 <memonote0880@gmail.com>
2025-08-09 11:43:10 +0900

    기본 데이터 형태

--- memory08.py
+++ memory08.py
@@ -7,3 +7,35 @@ x = {'a':100, 'b':200} #dictionary key:value
 print(x)
 print(x['a'])
 print(type(x))
+
+example = {
+    'python': [True, False, True, True, True, True, True, False, False, True],
+    'java': [True, False, False, True, True, True, False, False, False, True],
+    'git': [False, False, True, True, False, True, True, True, True, True],
+}
+
+python_description = [
+    {
+        'answer': 1,
+        'description': 'python에 대한 설명은 1번이 맞습니다.'
+    },
+    {
+        'answer': "list",
+        'description': 'python의 열거형 데이터 타입은 list입니다.'
+    },
+    {
+        'answer': True,
+        'description': 'python의 LIST 안에 Dictionary를 사용할 수 있습니다.'
+    },
+]
+
+list_example = [1, "+", 2, "="]
+
+print(list_example)
+print(list_example[1])
+
+#변할 수 있는 데이터는 key값으로 사용 불가능
+dict_example = {
+1: 'value 1'
+'a': 'value a',
+#
---
+++ variable.py
@@ -0,0 +1,10 @@
+print("첫번째 파이썬 파일")
+print("새로운 변경사항")
+print("새로운 변경사항222")
+
+user_input = int(input("숫자를 입력하고 엔터를 누르세요:")) # 1 "1"
+print(user_input+5)
+
+# 사용자에게 입력받은 값을 바로 정수로 바꾸어 user_input 변수에 대입
+user_input = int("숫자를 입력하고 엔터를 누르세요:")
+print(int(user_input)+5)
