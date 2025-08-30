bdab27b6c0cc139b91bed25d6184b93f8f8db13b
memory10614 <memonote0880@gmail.com>
2025-08-30 09:44:45 +0900

    new commit

--- opereter.py
+++ opereter.py
@@ -35,3 +35,16 @@ print("x // y : ", z)
 
 z = x ** y
 print("x ** y : ", z)
+
+str_x = "hello"
+str_y = "python"
+
+str_z = str_x + str_y
+print("str_x + str_y:", str_z)
+
+# unsupported operand type(s) for -: 'str' and 'str'
+# str_z = str_x - str_y
+# print("str_x + str_y:", str_z)
+
+str_z = str_x * 2
+print("str_x * 2", str_z)
