c3464478f6144980b89eb0c7559fbf861829d16e
memory10614 <memonote0880@gmail.com>
2025-08-30 09:30:55 +0900

    new commit

---
+++ opereter.py
@@ -0,0 +1,28 @@
+x = 10   #대입 연산자
+y = 15
+
+
+
+#조건 연산자
+z = x == y
+print("x == y : ", z)
+z = x != y
+print("x != y : ", z)
+
+#사칙연산
+z = x + y
+print("x + y : ", z)
+z = x - y
+print("x - y : ", z)
+z = x * y
+print("x * y : ", z)
+z = x / y
+print("x / y : ", z)
+
+z = x % y
+print("x % y : ", z)
+z = x // y
+print("x // y : ", z)
+
+z = x ** y
+print("x ** y : ", z)
