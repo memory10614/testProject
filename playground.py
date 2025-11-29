401e5314d00744856a0576e62456ce37bc6f1db5
memory10614 <memonote0880@gmail.com>
2025-11-29 10:46:02 +0900

    2025-11-29 문제풀이(수정본)

--- playground.py
+++ playground.py
@@ -1,15 +1,15 @@
 import random
 
 def UP_DOWN_게임():
- result = random.randint(1, 100)
- print("게임을 시작하지.")
+    result = random.randint(1, 100)
+    print("게임을 시작하지.")
 
-while True:
-    user = int(input("1부터 100까지의 아무 숫자나 입력하시오."))
-    if user < result:
-        print("up")
-    elif user > result:
-        print("down")
-    elif user == result:
-         print("게임을 종료하지.")
-         break
+    while True:
+        user = int(input("1부터 100까지의 아무 숫자나 입력하시오."))
+        if user < result:
+            print("up")
+        elif user > result:
+            print("down")
+        elif user == result:
+            print("게임을 종료하지.")
+            break
